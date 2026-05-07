dfd004de6106f9baff71b6f3a57bca4fac7e3260
ryuzaki <atiblpu@gmail.com>
2026-05-07 12:39:18 +0700

    feat:prefaring to adding  wd command

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { Telegraf } from 'telegraf';
+import { Telegraf } from "telegraf";
 
 // Buat instance bot Telegraf
 // BOT_TOKEN dibaca dari file .env
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process?.env.BOT_TOKEN);
 
 // Global error handler
 // Kalau ada command/handler yang error, ditangkap di sini
 bot.catch((err, ctx) => {
   console.error(`Bot error:`, err);
 });
-
 export default bot;
